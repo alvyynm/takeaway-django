e01c62ccb15cff6015729a4461f617b2fe5e7459
alvyynm <alvyynm@gmail.com>
2024-12-16 22:04:12 +0300

    feat: create dashboard view and url

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def dashboard_view(request):
+    """Dashboard view for the authenticated user"""
+    return render(request, 'dashboard/dashboard.html')
