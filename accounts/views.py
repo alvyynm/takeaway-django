21286f8f2be2666845b9aab2e12ddb3eb796546c
alvyynm <alvyynm@gmail.com>
2024-12-16 20:37:15 +0300

    feat: use custom user registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 
@@ -19,11 +20,11 @@ def dashboard_view(request):
 def signup_view(request):
     """User registration view"""
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
 
         if form.is_valid():
             form.save()
             return redirect('accounts:login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
