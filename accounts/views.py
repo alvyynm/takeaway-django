9d089fa99a255b8b784498f3501be4f5528fd373
alvyynm <alvyynm@gmail.com>
2024-12-16 16:08:49 +0300

    feat: create profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile_view(request):
+    """Profile view for the authenticated user"""
+    return render(request, 'accounts/profile.html')
