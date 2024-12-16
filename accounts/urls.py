354fe34c3d49ecc063b6d318f6a09dec6ab339f1
alvyynm <alvyynm@gmail.com>
2024-12-16 16:15:29 +0300

    feat: create dashboard view and url

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def profile_view(request):
     """Profile view for the authenticated user"""
     return render(request, 'accounts/profile.html')
+
+
+def dashboard_view(request):
+    """Dashboard view for the authenticated user"""
+    return render(request, 'accounts/dashboard.html')
