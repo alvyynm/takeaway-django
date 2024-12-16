76edc3e8ca0a54ee235d363161bbed94068f5599
alvyynm <alvyynm@gmail.com>
2024-12-16 16:29:16 +0300

    feat: require login to access dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -8,6 +9,7 @@ def profile_view(request):
     return render(request, 'accounts/profile.html')
 
 
+@login_required
 def dashboard_view(request):
     """Dashboard view for the authenticated user"""
     return render(request, 'accounts/dashboard.html')
