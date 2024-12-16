dc14ccecb69e39e728d2ba17702f19712c3cc8b7
alvyynm <alvyynm@gmail.com>
2024-12-16 21:05:39 +0300

    chore: install and configure django-simple-history

--- takeawaydjango/settings.py
+++ takeawaydjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'simple_history',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'takeawaydjango.urls'
