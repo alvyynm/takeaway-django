2cf102401642b4de46343491dad4e5c9550c70c1
alvyynm <alvyynm@gmail.com>
2024-12-16 18:05:36 +0300

    fix: specify templates folder for auth and reorder urls

--- takeawaydjango/settings.py
+++ takeawaydjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'takeawaydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'accounts/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
