61b0db626afa94f52e061152e50fde4e6c95fe51
alvyynm <alvyynm@gmail.com>
2024-12-16 16:09:19 +0300

    feat: add profile view url to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from . import views
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name='logout'),
+    path('profile/', views.profile_view, name='profile'),
 ]
